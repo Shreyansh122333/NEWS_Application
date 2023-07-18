9476dd2861804140c51da5c7c9d81671e553f93e
Shreyansh Kumar Singh <87147073+Shreyansh122333@users.noreply.github.com>
2023-07-18 18:04:40 +0530

    Update and rename script to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "55c21466e8284a2b9874ca04ed4490d7";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
